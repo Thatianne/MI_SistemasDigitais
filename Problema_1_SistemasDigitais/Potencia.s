0c8516d219c712d59170d47b27463781fe8e8f1a
Thatianne Cristina <thatianne.cristina@hotmail.com>
2017-04-15 17:34:13 -0300

    Cálculo de potência por entrada serial

--- Problema_1_SistemasDigitais/Potencia.s
+++ Problema_1_SistemasDigitais/Potencia.s
@@ -1,20 +1,43 @@
 
 .data
 	resultado: .word 0
+	.equ UART0, 0x860
 
 .text
 	.global main
+
 	main:
-	addi r4, r0, 3 #base
-	addi r5, r0, 4 #expoente
-	addi r7, r0, 1 #contador para laco
-	add r8, r0, r4 #resultado
 
-	rep: mul r8, r8, r4
-		 addi r7, r7, 1
-	bne r7, r5, rep
+		base:
+			movia r4, UART0
+			call nr_uart_rxchar
+			blt r2, r0, base
+		mov r6, r2 # r6 tem o valor da base vindo do uart_0
 
-	# se der diferente guarda o valor na memoria
-	movia r9, resultado
-	stw r8, 0(r9)
+		# limpar uart_0 para nao passar o mesmo valor da base para o expoente
+		limp:
+			movia r2, UART0
+			call nr_uart_rxchar
+			blt r2, r0, limp
+
+		exp:
+			movia r5, UART0
+			call nr_uart_rxchar
+			blt r2, r0, exp
+		mov r5, r2 # r5 tem o valor do expoente vindo do uart_0
+
+
+		subi r6, r6, 48
+		subi r5, r5, 48
+
+		addi r7, r0, 1 #contador para laco
+		add r8, r0, r6 #resultado
+
+		rep: mul r8, r8, r6
+			 addi r7, r7, 1
+		bne r7, r5, rep
+
+		# se o contador der diferente do expoente guarda o valor na memoria
+		movia r9, resultado
+		stw r8, 0(r9)
 
